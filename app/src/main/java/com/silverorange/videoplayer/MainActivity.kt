730868a72e09a0b359629be046d2394f9ca384f5
keiwu <keiwus@gmail.com>
2023-05-07 23:13:30 -0700

    pause the video when app is not in foreground

--- app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -20,21 +20,27 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.ui.PlayerView
 import com.silverorange.videoplayer.ui.VideoViewModel
 import com.silverorange.videoplayer.util.VideoEvent
@@ -87,6 +93,23 @@ fun VideoPresenter(){
     }
   }
 
+  var lifecycle by remember {
+    mutableStateOf(Lifecycle.Event.ON_CREATE)
+  }
+  val lifecycleOwner = LocalLifecycleOwner.current
+
+  DisposableEffect(lifecycleOwner) {
+    val observer = LifecycleEventObserver { _, event ->
+      lifecycle = event
+    }
+    lifecycleOwner.lifecycle.addObserver(observer)
+
+    onDispose {
+      lifecycleOwner.lifecycle.removeObserver(observer)
+    }
+  }
+
+
   Column(
     modifier = Modifier
       .fillMaxSize()
@@ -102,6 +125,22 @@ fun VideoPresenter(){
           PlayerView(context).also {
             it.player = videoViewModel.player
             it.useController = false
+          }
+        },
+        update = {
+          when (lifecycle) {
+            Lifecycle.Event.ON_PAUSE -> {
+              it.onPause()
+              it.player?.pause()
+            }
+            Lifecycle.Event.ON_RESUME -> {
+              it.onResume()
+            }
+            Lifecycle.Event.ON_STOP -> {
+              //pause the video instead stop
+              it.player?.pause()
+            }
+            else -> Unit
           }
         },
         modifier = Modifier
